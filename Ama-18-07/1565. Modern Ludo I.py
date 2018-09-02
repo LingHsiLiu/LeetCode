6437fb98dc8484b25f135afbeb4967af99a205bd
Althea <lhliu2@illinois.edu>
2018-09-01 20:13:24 -0500

    Update 1565. Modern Ludo I.py

--- Ama-18-07/1565. Modern Ludo I.py
+++ Ama-18-07/1565. Modern Ludo I.py
@@ -1,16 +1,16 @@
-There is a one-dimensional board with a starting point on the far left side of the board and an end point on the far right side of the board. There are several positions on the board that are connected to other positions, ie if A is connected to B, then when chess falls at position A, you can choose whether to move the chess from A to B. And the connection is one way, which means that the chess cannot move from B to A. Now you have a six-sided dice, find the minimum steps to reach the end.
+# There is a one-dimensional board with a starting point on the far left side of the board and an end point on the far right side of the board. There are several positions on the board that are connected to other positions, ie if A is connected to B, then when chess falls at position A, you can choose whether to move the chess from A to B. And the connection is one way, which means that the chess cannot move from B to A. Now you have a six-sided dice, find the minimum steps to reach the end.
 
-Example
-input:
-length = 10
-connections = [[2, 10]]
-output:
-1
-Notice
-the index starts from 1.
-length > 1
-The starting point is not connected to any other location
-connections[i][0] < connections[i][1]
+# Example
+# input:
+# length = 10
+# connections = [[2, 10]]
+# output:
+# 1
+# Notice
+# the index starts from 1.
+# length > 1
+# The starting point is not connected to any other location
+# connections[i][0] < connections[i][1]
 
 class Solution:
     """
