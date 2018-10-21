b95abb4f1617cb085a194d8af0cf68f065248db5
Althea <lhliu2@illinois.edu>
2018-10-21 16:52:21 -0500

    Create 20. Valid Parentheses.py

---
+++ LinkedIn/20. Valid Parentheses.py
@@ -0,0 +1,34 @@
+#LeetCode #20
+#LintCode #423
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
+
+
+
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def isValidParentheses(self, s):
+        # write your code here
+        stack = []
+        for ch in s:
+            # 压栈
+            if ch == '{' or ch == '[' or ch == '(':
+                stack.append(ch)
+            else:
+                # 栈需非空
+                if not stack:
+                    return False
+                # 判断栈顶是否匹配
+                if ch == ']' and stack[-1] != '[' or ch == ')' and stack[-1] != '(' or ch == '}' and stack[-1] != '{':
+                    return False
+                # 弹栈
+                stack.pop()
+        return not stack
