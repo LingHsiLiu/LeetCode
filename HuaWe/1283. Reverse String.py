196d0335a0af7c4e2182a8602767907828203adb
Althea <lhliu2@illinois.edu>
2018-12-22 16:38:06 -0600

    Create 1283. Reverse String.py

---
+++ HuaWe/1283. Reverse String.py
@@ -0,0 +1,15 @@
+# 1283. Reverse String
+# Write a function that takes a string as input and returns the string reversed.
+
+# Example
+# Given s = "hello"
+# return "olleh".
+
+class Solution:
+    """
+    @param s: a string
+    @return: return a string
+    """
+    def reverseString(self, s):
+        # write your code here
+        return s[::-1]
